0d4a9f0ed5bd34f46a53e3f3c9b67d55cfe45379
Romesh Rayen <rgrayen94@gmail.com>
2026-05-17 21:48:04 +0530

    Launch ready LaundryYalu app

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,5 +1,6 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyATmPMw_qmfptdoAkrP3iV_UgdHHIN2qwA",
@@ -8,8 +9,12 @@ const firebaseConfig = {
   storageBucket: "laundry-yalu.firebasestorage.app",
   messagingSenderId: "341774885799",
   appId: "1:341774885799:web:0ebf9cb2a87958cb2b4c82",
+  measurementId: "G-GW6LMWEWZY"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length
+  ? getApp()
+  : initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
